c1eb0b8dafdf86ee42dee5b58db71a64ef04fcce
eMobilis <tonykaranja15@gmail.com>
2023-03-28 15:36:13 +0300

    my commit

--- tonycrudesystem/urls.py
+++ tonycrudesystem/urls.py
@@ -19,5 +19,7 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.displayindex, name='index')
+    path('', views.index, name='index'),
+    path('delete/<id>', views.deleteData, name='deletedata'),
+    path('insert', views.insertData, name='insertdata')
 ]
--- tonycrudesystem/views.py
+++ tonycrudesystem/views.py
@@ -1,3 +1,26 @@
+from .models import People
 from django.shortcuts import render, redirect
-def displayindex(request):
+
+def insertData(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        idnum = request.POST.get('idnum')
+
+        query = People(name=name, email=email, age=age, gender=gender, idnum=idnum)
+        query.save()
+        return redirect("/")
+
+    return render(request, 'index.html')
+
+def index(request):
+    data = People.objects.all()
+    context = {"data": data}
+    return render(request, 'index.html', context)
+def deleteData(request, id):
+    d = People.objects.get(id=id)
+    d.delete()
+    return redirect("/")
     return render(request, "index.html")
